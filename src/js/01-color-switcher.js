2043532f4a682e62cd1642f72d1e08f28187e81e
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-07 21:55:56 +0300

    Start Task-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,10 @@ const body = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 let timerId = null;
 
 startBtn.addEventListener("click", onStartBtnClick);
@@ -26,6 +30,3 @@ function onStopBtnClick() {
     stopBtn.setAttribute("disabled", '');
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
