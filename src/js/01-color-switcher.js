872268c247503790f57028ecf53416bb5ae387df
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-07 19:31:19 +0300

    Task-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener("click", onStartBtnClick);
+stopBtn.addEventListener("click", onStopBtnClick);
+
+stopBtn.setAttribute("disabled", '');
+
+function onStartBtnClick() {
+    startBtn.setAttribute("disabled", '');
+    stopBtn.removeAttribute("disabled");
+
+    timerId = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`;
+        console.log('Змінюємо колір');                
+    }, 1000);
+}
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    console.log('Зупиняємо зміну кольору')
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", '');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
