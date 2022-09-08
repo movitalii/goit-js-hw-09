d7e77ba5f72b10ffde71812a9f8e98d0261acb89
movitalii <103452623+movitalii@users.noreply.github.com>
2022-09-08 12:56:30 +0000

    Deploying to gh-pages from @ movitalii/goit-js-hw-09@2ab7ae62b4f2e31518d570f97df5be7b652d3950 🚀

--- 02-timer.290403be.js
+++
@@ -1,2 +0,0 @@
-const o=new Date;console.log(o);
-//# sourceMappingURL=02-timer.290403be.js.map
--- 02-timer.3d45a6ab.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=new Date;console.log(n)}();
-//# sourceMappingURL=02-timer.3d45a6ab.js.map
