2ab7ae62b4f2e31518d570f97df5be7b652d3950
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-08 15:55:53 +0300

    start Task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,50 @@
-const date = new Date();
-console.log(date)
+
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {                            
+  },
+};
+
+const input = document.querySelector("#datetime-picker");
+const fp = flatpickr(input, options);
+
+input.addEventListener("input", onInput);
+
+function onInput(event) {
+    const selectedTime = new Date(`${event.currentTarget.value}`)
+    const selectetimeInMs = selectedTime.getTime();
+    console.log(selectetimeInMs);
+
+    const currentTime = Date.now();
+        setInterval(() => {
+        const currentTime = Date.now();
+        const clock = selectetimeInMs - currentTime;
+        console.log('time left:', clock)            
+    }, 1000);   
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
