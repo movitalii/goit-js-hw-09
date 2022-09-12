e81cb78998b3df3107ece8f35c8d018e53261d83
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-12 14:16:30 +0300

    hw-9 to check

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -30,7 +30,7 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
-    setTimeout(delay => {
+    setTimeout(()=> {
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
